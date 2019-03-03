6825bf890d8d523376d8296ca6a4733d511e59bb
Anuraj <anuraj.p@msn.com>
2019-03-03 16:49:03 +0530

    Initial Commit

---
+++ Chapter2/Draken.Web/Draken.Data/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Draken.Data
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ Chapter2/Draken.Web/Draken.Data/Validators/BusinessOnlyEmailAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Draken.Data.Validators
+{
+    public class BusinessOnlyEmailAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var email = value.ToString().ToLower();
+            return !email.Contains("hotmail") && !email.Contains("gmail");
+        }
+    }
+}
