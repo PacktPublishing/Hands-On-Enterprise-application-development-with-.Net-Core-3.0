0808e880b3f7c45cdf8d1eb6ad96580e261b6705
Karthick <me@karthi-keyan.com>
2019-04-20 14:02:01 +0530

    initial commit

---
+++ Chapter4/Draken.CRM/Draken.Repository/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Draken.Models;
+
+namespace Draken.Repository
+{
+    public interface IContactRepository
+    {
+        IList<Contact> GetAll();
+    }
+
+    public class ContactRepository : IContactRepository
+    {
+        public ContactRepository()
+        {
+        }
+
+        public IList<Contact> GetAll()
+        {
+            var contacts = new List<Contact>
+            {
+                new Contact() { FirstName = "Andrew", LastName = "Mathew", Email = "andrew@hotmail.com" }
+            };
+
+            return contacts;
+        }
+    }
+
+}
---
+++ Chapter4/Draken.CRM/Draken.Web/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Draken.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Draken.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        readonly IContactRepository contactRepository;
+
+        public ContactController(IContactRepository contactRepository)
+        {
+            this.contactRepository = contactRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
